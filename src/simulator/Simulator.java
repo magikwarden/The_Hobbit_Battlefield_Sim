d4f014e70d41078792f4a99cd438f9d0eaf6356d
Calvin <Calvin@MAGIK-PC>
2018-10-02 22:01:43 -0400

    all formatted

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -15,157 +15,180 @@ import actor.Actor;
 import actor.ActorFactory;
 import army.Army;
 
-public class Simulator extends Group {
-	private Stage primaryStage;
-	private Army forcesOfLight;
-	private Army forcesOfDarkness;
-	private Stage stageListControllerWindow;
-	private Stage stageTableControllerWindow;
-	/**
-	 * this sets up the forces when simulator is ran ssetting their initial values
-	 * @param primaryStage
-	 */
-	public Simulator(Stage primaryStage) {
-		this.primaryStage = primaryStage;
-		this.forcesOfLight = new Army("Forces of Light", this, Color.AQUA);
-		this.forcesOfDarkness = new Army("Forces of Darkness", this, Color.RED);
-		this.forcesOfDarkness.setOpposingArmy(forcesOfLight);
-		this.forcesOfLight.setOpposingArmy(forcesOfDarkness);
-		this.buildListViewWindow();
-		this.buildTableViewWindow();
-	}
-	/**
-	 * when called it popluates the screen with actors
-	 */
-	public void populate() {
-		this.forcesOfLight.populate(ActorFactory.Type.HOBBIT, 2);
-		this.forcesOfLight.populate(ActorFactory.Type.WIZARD, 8);
-		this.forcesOfDarkness.populate(ActorFactory.Type.NAZGUL, 4);
-		this.forcesOfDarkness.populate(ActorFactory.Type.ORC, 4);
-	}
-	/**
-	 * when called actors start moving
-	 */
-	public void run() {
-		this.forcesOfLight.startMoving();
-		this.forcesOfDarkness.startMoving();
-	}
-	/**
-	 * when called actors stop moving
-	 */
-	public void suspend() {
-		this.forcesOfLight.suspendMoving();
-		this.forcesOfDarkness.suspendMoving();
+public class Simulator extends Group
+{
+    private Stage primaryStage;
+    private Army forcesOfLight;
+    private Army forcesOfDarkness;
+    private Stage stageListControllerWindow;
+    private Stage stageTableControllerWindow;
 
-	}
-	/**
-	 * when called displays list view window of actors
-	 */
-	public void openListViewWindow() {
-		this.stageListControllerWindow.show();
-	}
-	/**
-	 * when called displays table view window of actors
-	 */
-	public void openTableViewWindow() {
-		this.stageTableControllerWindow.show();
+    /**
+     * this sets up the forces when simulator is ran ssetting their initial values
+     * 
+     * @param primaryStage
+     */
+    public Simulator(Stage primaryStage)
+    {
+	this.primaryStage = primaryStage;
+	this.forcesOfLight = new Army("Forces of Light", this, Color.AQUA);
+	this.forcesOfDarkness = new Army("Forces of Darkness", this, Color.RED);
+	this.forcesOfDarkness.setOpposingArmy(forcesOfLight);
+	this.forcesOfLight.setOpposingArmy(forcesOfDarkness);
+	this.buildListViewWindow();
+	this.buildTableViewWindow();
+    }
 
-	}
-	/**
-	 * use for saving actors on the screen for later
-	 */
-	public void save() {
-		// Using a try block in case there is a file I/O error. Open a file that
-		// is configured for binary output.
-		try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("battlefield.ser"))) 
-		{
-			this.forcesOfLight.serialize(out);// "normal" method call that I created. Army class NOT serializable.
-			this.forcesOfDarkness.serialize(out);// Actor class and ALL its subclasses are serializable.
-		} 
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-		}
-	} // end save()
-	/**
-	 * use to read or open saved acotrs on the screen to display
-	 */
-	public void restore() {
-		// Using a try block in case there is a file I/O error. Open a file that
-		// is configured for binary input.
-		try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("battlefield.ser"))) 
-		{
-			this.forcesOfLight.deserialize(in);// "normal" method call that I created. Army class NOT serializable. 
-			this.forcesOfDarkness.deserialize(in);// Actor class and ALL its subclasses are serializable.
-		} 
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-		}
-	} // end restore()
-	/**
-	 * get force of light actors
-	 * @return
-	 */
-	public Army getForceOfLight()
+    /**
+     * when called it popluates the screen with actors
+     */
+    public void populate()
+    {
+	this.forcesOfLight.populate(ActorFactory.Type.HOBBIT, 2);
+	this.forcesOfLight.populate(ActorFactory.Type.WIZARD, 8);
+	this.forcesOfDarkness.populate(ActorFactory.Type.NAZGUL, 4);
+	this.forcesOfDarkness.populate(ActorFactory.Type.ORC, 4);
+    }
+
+    /**
+     * when called actors start moving
+     */
+    public void run()
+    {
+	this.forcesOfLight.startMoving();
+	this.forcesOfDarkness.startMoving();
+    }
+
+    /**
+     * when called actors stop moving
+     */
+    public void suspend()
+    {
+	this.forcesOfLight.suspendMoving();
+	this.forcesOfDarkness.suspendMoving();
+
+    }
+
+    /**
+     * when called displays list view window of actors
+     */
+    public void openListViewWindow()
+    {
+	this.stageListControllerWindow.show();
+    }
+
+    /**
+     * when called displays table view window of actors
+     */
+    public void openTableViewWindow()
+    {
+	this.stageTableControllerWindow.show();
+
+    }
+
+    /**
+     * use for saving actors on the screen for later
+     */
+    public void save()
+    {
+	// Using a try block in case there is a file I/O error. Open a file that
+	// is configured for binary output.
+	try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("battlefield.ser")))
 	{
-		return this.forcesOfLight;
-	}
-	/**
-	 * get force of darkness actors
-	 * @return
-	 */
-	public Army getForceOfDarkness()
+	    this.forcesOfLight.serialize(out);// "normal" method call that I created. Army class NOT serializable.
+	    this.forcesOfDarkness.serialize(out);// Actor class and ALL its subclasses are serializable.
+	} catch (Exception e)
 	{
-		return this.forcesOfDarkness;
+	    e.printStackTrace();
 	}
-	/**
-	 * helps display list view window of actors
-	 */
-	public final void buildListViewWindow() { // final because of its use in
-												// the constructor
-		final double PREF_WIDTH = 700.0;
-		ListView<Actor> 
-		listView = new ListView<Actor>(this.forcesOfLight.getCollectionActors());
-		listView.setPrefWidth(PREF_WIDTH);
-		VBox vBoxLightArmy = new VBox(5.0, new Text(this.forcesOfLight.getName()),listView);
-		listView = new ListView<Actor>(this.forcesOfDarkness.getCollectionActors());
-		listView.setPrefWidth(PREF_WIDTH);
-		VBox vBoxDarkArmy = new VBox(5.0, new Text(this.forcesOfDarkness.getName()),listView);
-		HBox hBoxSceneGraphRoot = new HBox(5.0, vBoxLightArmy, vBoxDarkArmy);
+    } // end save()
 
-		if (this.stageListControllerWindow != null) 
-		{
-			this.stageListControllerWindow.close();
-			this.stageListControllerWindow.setScene(null);
-		}
-		this.stageListControllerWindow = new Stage(StageStyle.UTILITY);
-		this.stageListControllerWindow.initOwner(this.primaryStage);
-		this.stageListControllerWindow.setScene(new Scene(hBoxSceneGraphRoot));
-	} // end buildListViewWindow()
-	/**
-	 * helps display table view windows for actors
-	 */
-	public final void buildTableViewWindow() { // final because of its use in
-												// the constructor
-		final double PREF_WIDTH = 470.0;
-		final double PREF_HEIGHT = 450.0;
-		VBox vBoxLightArmy = new VBox
-		(10.0, new Text(this.forcesOfLight.getName()), this.forcesOfLight.getTableViewOfActors());
-		vBoxLightArmy.setPrefWidth(PREF_WIDTH);
-		VBox vBoxDarkArmy = new VBox
-		(10.0,new Text(this.forcesOfDarkness.getName()),this.forcesOfDarkness.getTableViewOfActors());
-		vBoxDarkArmy.setPrefWidth(PREF_WIDTH);
-		HBox hBoxSceneGraphRoot = new HBox(10.0, vBoxLightArmy, vBoxDarkArmy);
-		hBoxSceneGraphRoot.setPrefWidth((PREF_WIDTH + 20) * 2);
-		hBoxSceneGraphRoot.setPrefHeight(PREF_HEIGHT);
-		if (this.stageTableControllerWindow != null) 
-		{
-			this.stageTableControllerWindow.close();
-			this.stageTableControllerWindow.setScene(null);
-		}
-		this.stageTableControllerWindow = new Stage(StageStyle.UTILITY);
-		this.stageTableControllerWindow.initOwner(this.primaryStage);
-		this.stageTableControllerWindow.setScene(new Scene(hBoxSceneGraphRoot));
-	} // end buildTableViewWindow()
+    /**
+     * use to read or open saved acotrs on the screen to display
+     */
+    public void restore()
+    {
+	// Using a try block in case there is a file I/O error. Open a file that
+	// is configured for binary input.
+	try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("battlefield.ser")))
+	{
+	    this.forcesOfLight.deserialize(in);// "normal" method call that I created. Army class NOT serializable.
+	    this.forcesOfDarkness.deserialize(in);// Actor class and ALL its subclasses are serializable.
+	} catch (Exception e)
+	{
+	    e.printStackTrace();
+	}
+    } // end restore()
+
+    /**
+     * get force of light actors
+     * 
+     * @return
+     */
+    public Army getForceOfLight()
+    {
+	return this.forcesOfLight;
+    }
+
+    /**
+     * get force of darkness actors
+     * 
+     * @return
+     */
+    public Army getForceOfDarkness()
+    {
+	return this.forcesOfDarkness;
+    }
+
+    /**
+     * helps display list view window of actors
+     */
+    public final void buildListViewWindow()
+    { // final because of its use in
+      // the constructor
+	final double PREF_WIDTH = 700.0;
+	ListView<Actor> listView = new ListView<Actor>(this.forcesOfLight.getCollectionActors());
+	listView.setPrefWidth(PREF_WIDTH);
+	VBox vBoxLightArmy = new VBox(5.0, new Text(this.forcesOfLight.getName()), listView);
+	listView = new ListView<Actor>(this.forcesOfDarkness.getCollectionActors());
+	listView.setPrefWidth(PREF_WIDTH);
+	VBox vBoxDarkArmy = new VBox(5.0, new Text(this.forcesOfDarkness.getName()), listView);
+	HBox hBoxSceneGraphRoot = new HBox(5.0, vBoxLightArmy, vBoxDarkArmy);
+
+	if (this.stageListControllerWindow != null)
+	{
+	    this.stageListControllerWindow.close();
+	    this.stageListControllerWindow.setScene(null);
+	}
+	this.stageListControllerWindow = new Stage(StageStyle.UTILITY);
+	this.stageListControllerWindow.initOwner(this.primaryStage);
+	this.stageListControllerWindow.setScene(new Scene(hBoxSceneGraphRoot));
+    } // end buildListViewWindow()
+
+    /**
+     * helps display table view windows for actors
+     */
+    public final void buildTableViewWindow()
+    { // final because of its use in
+      // the constructor
+	final double PREF_WIDTH = 470.0;
+	final double PREF_HEIGHT = 450.0;
+	VBox vBoxLightArmy = new VBox(10.0, new Text(this.forcesOfLight.getName()),
+		this.forcesOfLight.getTableViewOfActors());
+	vBoxLightArmy.setPrefWidth(PREF_WIDTH);
+	VBox vBoxDarkArmy = new VBox(10.0, new Text(this.forcesOfDarkness.getName()),
+		this.forcesOfDarkness.getTableViewOfActors());
+	vBoxDarkArmy.setPrefWidth(PREF_WIDTH);
+	HBox hBoxSceneGraphRoot = new HBox(10.0, vBoxLightArmy, vBoxDarkArmy);
+	hBoxSceneGraphRoot.setPrefWidth((PREF_WIDTH + 20) * 2);
+	hBoxSceneGraphRoot.setPrefHeight(PREF_HEIGHT);
+	if (this.stageTableControllerWindow != null)
+	{
+	    this.stageTableControllerWindow.close();
+	    this.stageTableControllerWindow.setScene(null);
+	}
+	this.stageTableControllerWindow = new Stage(StageStyle.UTILITY);
+	this.stageTableControllerWindow.initOwner(this.primaryStage);
+	this.stageTableControllerWindow.setScene(new Scene(hBoxSceneGraphRoot));
+    } // end buildTableViewWindow()
 }
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,145 +1,200 @@
 package util;
 
-
 import java.util.Scanner;
 
 /**
- * <i>Singleton</i> class to assist with basic keyboard input operations. Only a single <i>Input</i> object will ever be created. The <i>Input</i> class clusters related input operations and will build one <i>Scanner</i> object to use with input.
- * <i>Scanner</i> objects are big and complex; thus, this approach reduces the overhead associated with the repeated creation of <i>Scanner</i> objects.
+ * <i>Singleton</i> class to assist with basic keyboard input operations. Only a
+ * single <i>Input</i> object will ever be created. The <i>Input</i> class
+ * clusters related input operations and will build one <i>Scanner</i> object to
+ * use with input. <i>Scanner</i> objects are big and complex; thus, this
+ * approach reduces the overhead associated with the repeated creation of
+ * <i>Scanner</i> objects.
  * 
  * @author Rex Woollard
  */
-public final class Input {
-	/** Keyword <i>static</i> makes this a <i>class</i> oriented variable, rather than <i>object</i> oriented variable; only one instance of an <i>Input</i> object will ever exist, and the instance is tracked by this reference variable. */ 
-	public final static Input instance = new Input();
-	/** Each input method allows for a number failed attempts before throwing an exception. */
-	public final static int MAX_ATTEMPTS = 3;
-	/** Object-oriented instance variable, but since only one <i>Input</i> can ever be created, only one <i>Scanner</i> object will ever be created. */
-	private Scanner scannerKeyboard;
+public final class Input
+{
+    /**
+     * Keyword <i>static</i> makes this a <i>class</i> oriented variable, rather
+     * than <i>object</i> oriented variable; only one instance of an <i>Input</i>
+     * object will ever exist, and the instance is tracked by this reference
+     * variable.
+     */
+    public final static Input instance = new Input();
+    /**
+     * Each input method allows for a number failed attempts before throwing an
+     * exception.
+     */
+    public final static int MAX_ATTEMPTS = 3;
+    /**
+     * Object-oriented instance variable, but since only one <i>Input</i> can ever
+     * be created, only one <i>Scanner</i> object will ever be created.
+     */
+    private Scanner scannerKeyboard;
 
-	/** A <i>private</i> constructor guarantees that no <i>Input</i> object can be created from outside the <i>Input</i> class; this is essential for the <i>singleton</i> design pattern. */
-	private Input() {	
-		scannerKeyboard = new Scanner(System.in);
-		}
+    /**
+     * A <i>private</i> constructor guarantees that no <i>Input</i> object can be
+     * created from outside the <i>Input</i> class; this is essential for the
+     * <i>singleton</i> design pattern.
+     */
+    private Input()
+    {
+	scannerKeyboard = new Scanner(System.in);
+    }
 
-	/**
-	 * Presents a prompt to the user and retrieves an <i>double</i> value. 
-	 * @param prompt   reference to a <i>String</i> object whose contents will be displayed to the user as a prompt.
-	 * @return <i>double</i> value input from keyboard
-	 */
-	public double getDouble(String prompt) throws NumberFormatException, RuntimeException {
-		System.out.print(prompt);
-		int numAttempts = 0;
-		do {
-			try {
-				++numAttempts;
-				String stringInput = scannerKeyboard.nextLine();
-				return Double.parseDouble(stringInput);
-			} catch (NumberFormatException nfe) {
-				if (numAttempts >= MAX_ATTEMPTS)
-					return 0.0;
-				System.out.format("Invalid double: %s", prompt);
-			}
-		} while(numAttempts < MAX_ATTEMPTS);
-		throw new RuntimeException("Too many failed attempts.");
-	} // end double getDouble()
+    /**
+     * Presents a prompt to the user and retrieves an <i>double</i> value.
+     * 
+     * @param prompt reference to a <i>String</i> object whose contents will be
+     *               displayed to the user as a prompt.
+     * @return <i>double</i> value input from keyboard
+     */
+    public double getDouble(String prompt) throws NumberFormatException, RuntimeException
+    {
+	System.out.print(prompt);
+	int numAttempts = 0;
+	do
+	{
+	    try
+	    {
+		++numAttempts;
+		String stringInput = scannerKeyboard.nextLine();
+		return Double.parseDouble(stringInput);
+	    } catch (NumberFormatException nfe)
+	    {
+		if (numAttempts >= MAX_ATTEMPTS)
+		    return 0.0;
+		System.out.format("Invalid double: %s", prompt);
+	    }
+	} while (numAttempts < MAX_ATTEMPTS);
+	throw new RuntimeException("Too many failed attempts.");
+    } // end double getDouble()
 
-	/**
-	 * Presents a prompt to the user and retrieves a <i>double</i> value which is within the range of <i>low</i> to <i>high</i> (inclusive). 
-	 * @param prompt reference to a <i>String</i> object whose contents will be displayed to the user as a prompt.
-	 * @param low lower boundary on the range of legitimate values
-	 * @param high upper boundary on the range of legitimate values
-	 * @return <i>double</i> value input from keyboard
-	 */
-	public double getDouble(String prompt, double low, double high) throws NumberFormatException, RuntimeException {
-		int numAttempts = 0;
-		double returnValue;
-		do {
-			System.out.printf("%s (%.1f-%.1f): ", prompt, low, high);
-			try {
-				++numAttempts;
-				String stringInput = scannerKeyboard.nextLine();
-				returnValue = Double.parseDouble(stringInput);
-				if (returnValue >= low && returnValue <= high)
-					return returnValue;
-			} catch (NumberFormatException nfe) {
-				if (numAttempts >= MAX_ATTEMPTS)
-					return low; // snap to the lowest legitimate value
-			}
-			System.out.println("Invalid double.");
-		} while (numAttempts < MAX_ATTEMPTS);
-		throw new RuntimeException("Too many failed attempts.");
-	} // end double getDouble()
+    /**
+     * Presents a prompt to the user and retrieves a <i>double</i> value which is
+     * within the range of <i>low</i> to <i>high</i> (inclusive).
+     * 
+     * @param prompt reference to a <i>String</i> object whose contents will be
+     *               displayed to the user as a prompt.
+     * @param low    lower boundary on the range of legitimate values
+     * @param high   upper boundary on the range of legitimate values
+     * @return <i>double</i> value input from keyboard
+     */
+    public double getDouble(String prompt, double low, double high) throws NumberFormatException, RuntimeException
+    {
+	int numAttempts = 0;
+	double returnValue;
+	do
+	{
+	    System.out.printf("%s (%.1f-%.1f): ", prompt, low, high);
+	    try
+	    {
+		++numAttempts;
+		String stringInput = scannerKeyboard.nextLine();
+		returnValue = Double.parseDouble(stringInput);
+		if (returnValue >= low && returnValue <= high)
+		    return returnValue;
+	    } catch (NumberFormatException nfe)
+	    {
+		if (numAttempts >= MAX_ATTEMPTS)
+		    return low; // snap to the lowest legitimate value
+	    }
+	    System.out.println("Invalid double.");
+	} while (numAttempts < MAX_ATTEMPTS);
+	throw new RuntimeException("Too many failed attempts.");
+    } // end double getDouble()
 
-	/**
-	 * Presents a prompt to the user and retrieves an <i>int</i> value. 
-	 * @param prompt   reference to a <i>String</i> object whose contents will be displayed to the user as a prompt.
-	 * @return <i>int</i> value input from keyboard
-	 */
-	public int getInt(String prompt) throws NumberFormatException, RuntimeException {
-		System.out.print(prompt);
-		int numAttempts = 0;
-		do {
-			try {
-				++numAttempts;
-				String stringInput = scannerKeyboard.nextLine();
-				return Integer.parseInt(stringInput);
-			} catch (NumberFormatException nfe) {
-				if (numAttempts >= MAX_ATTEMPTS)
-					return 0; // snap to 0
-				System.out.format("Invalid integer: %s", prompt);
-			}
-		} while(numAttempts < MAX_ATTEMPTS);
-		throw new RuntimeException("Too many failed attempts.");
-	} // end int getInt()
+    /**
+     * Presents a prompt to the user and retrieves an <i>int</i> value.
+     * 
+     * @param prompt reference to a <i>String</i> object whose contents will be
+     *               displayed to the user as a prompt.
+     * @return <i>int</i> value input from keyboard
+     */
+    public int getInt(String prompt) throws NumberFormatException, RuntimeException
+    {
+	System.out.print(prompt);
+	int numAttempts = 0;
+	do
+	{
+	    try
+	    {
+		++numAttempts;
+		String stringInput = scannerKeyboard.nextLine();
+		return Integer.parseInt(stringInput);
+	    } catch (NumberFormatException nfe)
+	    {
+		if (numAttempts >= MAX_ATTEMPTS)
+		    return 0; // snap to 0
+		System.out.format("Invalid integer: %s", prompt);
+	    }
+	} while (numAttempts < MAX_ATTEMPTS);
+	throw new RuntimeException("Too many failed attempts.");
+    } // end int getInt()
 
-	/**
-	 * Presents a prompt to the user and retrieves an <i>int</i> value which is within the range of <i>low</i> to <i>high</i> (inclusive). 
-	 * @param prompt reference to a <i>String</i> object whose contents will be displayed to the user as a prompt.
-	 * @param low lower boundary on the range of legitimate values
-	 * @param high upper boundary on the range of legitimate values
-	 * @return <i>int</i> value input from keyboard
-	 */
-	public int getInt(String prompt, int low, int high) throws NumberFormatException, RuntimeException {
-		int numAttempts = 0;
-		int returnValue;
-		do {
-			System.out.printf("%s (%d-%d): ", prompt, low, high);
-			try {
-				++numAttempts;
-				String stringInput = scannerKeyboard.nextLine();
-				returnValue = Integer.parseInt(stringInput);
-				if (returnValue >= low && returnValue <= high)
-					return returnValue;
-			} catch (NumberFormatException nfe) {
-				if (numAttempts >= MAX_ATTEMPTS)
-					return low; // snap to the lowest legitimate value
-			}
-			System.out.println("Invalid integer.");
-		} while (numAttempts < MAX_ATTEMPTS);
-		throw new RuntimeException("Too many failed attempts.");
-	} // end int getInt()
+    /**
+     * Presents a prompt to the user and retrieves an <i>int</i> value which is
+     * within the range of <i>low</i> to <i>high</i> (inclusive).
+     * 
+     * @param prompt reference to a <i>String</i> object whose contents will be
+     *               displayed to the user as a prompt.
+     * @param low    lower boundary on the range of legitimate values
+     * @param high   upper boundary on the range of legitimate values
+     * @return <i>int</i> value input from keyboard
+     */
+    public int getInt(String prompt, int low, int high) throws NumberFormatException, RuntimeException
+    {
+	int numAttempts = 0;
+	int returnValue;
+	do
+	{
+	    System.out.printf("%s (%d-%d): ", prompt, low, high);
+	    try
+	    {
+		++numAttempts;
+		String stringInput = scannerKeyboard.nextLine();
+		returnValue = Integer.parseInt(stringInput);
+		if (returnValue >= low && returnValue <= high)
+		    return returnValue;
+	    } catch (NumberFormatException nfe)
+	    {
+		if (numAttempts >= MAX_ATTEMPTS)
+		    return low; // snap to the lowest legitimate value
+	    }
+	    System.out.println("Invalid integer.");
+	} while (numAttempts < MAX_ATTEMPTS);
+	throw new RuntimeException("Too many failed attempts.");
+    } // end int getInt()
 
-	/**
-	 * Presents a prompt to the user and retrieves a <i>reference-to-String</i>. 
-	 * @param prompt reference to a <i>String</i> object whose contents will be displayed to the user as a prompt.
-	 * @return <i>reference-to-String</i> object created by keyboard input
-	 */
-	public String getString(String prompt) {
-		System.out.print(prompt);
-		//scannerKeyboard.useDelimiter("\r\n"); // Setting this delimiter ensures that we capture everything up to the <Enter> key. Without this, input stops at the next whitespace (space, tab, newline etc.).
-		String input = scannerKeyboard.nextLine();
-		//scannerKeyboard.reset(); // The preceding use of useDelimiter() changed the state of the Scanner object. reset() re-establishes the original state.
-		return input;
-	} // end String getString()
+    /**
+     * Presents a prompt to the user and retrieves a <i>reference-to-String</i>.
+     * 
+     * @param prompt reference to a <i>String</i> object whose contents will be
+     *               displayed to the user as a prompt.
+     * @return <i>reference-to-String</i> object created by keyboard input
+     */
+    public String getString(String prompt)
+    {
+	System.out.print(prompt);
+	// scannerKeyboard.useDelimiter("\r\n"); // Setting this delimiter ensures that
+	// we capture everything up to the <Enter> key. Without this, input stops at the
+	// next whitespace (space, tab, newline etc.).
+	String input = scannerKeyboard.nextLine();
+	// scannerKeyboard.reset(); // The preceding use of useDelimiter() changed the
+	// state of the Scanner object. reset() re-establishes the original state.
+	return input;
+    } // end String getString()
 
-	/**
-	 * Presents a prompt to the user and retrieves a <i>boolean</i> value. 
-	 * @param prompt reference to a <i>String</i> object whose contents will be displayed to the user as a prompt.
-	 * @return <i>boolean</i> value input from keyboard
-	 */
-	public boolean getBoolean(String prompt) {
-		System.out.print(prompt);
-		return scannerKeyboard.nextBoolean();
-	} // end boolean getBoolean()
+    /**
+     * Presents a prompt to the user and retrieves a <i>boolean</i> value.
+     * 
+     * @param prompt reference to a <i>String</i> object whose contents will be
+     *               displayed to the user as a prompt.
+     * @return <i>boolean</i> value input from keyboard
+     */
+    public boolean getBoolean(String prompt)
+    {
+	System.out.print(prompt);
+	return scannerKeyboard.nextBoolean();
+    } // end boolean getBoolean()
 } // end class Input
